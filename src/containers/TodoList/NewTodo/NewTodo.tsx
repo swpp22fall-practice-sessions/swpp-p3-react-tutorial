fb99d354615b8a7e48a5f9e9a78081f8b502ce47
Yujin Im <jina0605@gmail.com>
2022-09-21 20:32:06 +0900

    Check submitted at NewTodo

--- src/containers/TodoList/NewTodo/NewTodo.tsx
+++ src/containers/TodoList/NewTodo/NewTodo.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Navigate } from "react-router-dom";
 import "./NewTodo.css";
 
 export default function NewTodo() {
@@ -6,22 +7,26 @@ export default function NewTodo() {
   const [content, setContent] = useState<string>("");
   const [submitted, setSubmitted] = useState<boolean>(false);
 
-  return (
-    <div className="NewTodo">
-      <h1>Add a Todo</h1>
-      <label>Title</label>
-      <input
-        type="text"
-        value={title}
-        onChange={(event) => setTitle(event.target.value)}
-      />
-      <label>Content</label>
-      <textarea
-        rows={4}
-        value={content}
-        onChange={(event) => setContent(event.target.value)}
-      />
-      <button onClick={() => alert("Submitted")}>Submit</button>
-    </div>
-  );
+  if (submitted) {
+    return <Navigate to="/todos" />;
+  } else {
+    return (
+      <div className="NewTodo">
+        <h1>Add a Todo</h1>
+        <label>Title</label>
+        <input
+          type="text"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+        <label>Content</label>
+        <textarea
+          rows={4}
+          value={content}
+          onChange={(event) => setContent(event.target.value)}
+        />
+        <button onClick={() => alert("Submitted")}>Submit</button>
+      </div>
+    );
+  }
 }
