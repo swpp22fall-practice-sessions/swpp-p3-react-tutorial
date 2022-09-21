f97ff2d41aade94721170b33da5d94e3e3b88605
EngineerA <engineerA314@github.com>
2022-09-21 20:54:15 +0900

    using navigate, not useNavigate()

--- src/containers/TodoList/NewTodo/NewTodo.tsx
+++ src/containers/TodoList/NewTodo/NewTodo.tsx
@@ -1,19 +1,16 @@
 import { useState } from "react";
 import "./NewTodo.css";
 import { Navigate } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
 
 export default function NewTodo() {
   const [title, setTitle] = useState<string>("");
   const [content, setContent] = useState<string>("");
   const [submitted, setSubmitted] = useState<boolean>(false);
 
-  const navigate = useNavigate();
   const postTodoHandler = () => {
     const data = { title: title, content: content };
     alert("Submitted\n" + data.title + "\n" + data.content);
     setSubmitted(true);
-    navigate("/todos");
   };
 
   if (submitted) {
