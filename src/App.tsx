01ea2db8dc4d1ebd14d4f3885cc0cdcd9dd29abd
Yujin Im <jina0605@gmail.com>
2022-09-21 20:01:58 +0900

    Implement Todo.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import TodoList from "./containers/TodoList/TodoList";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <TodoList title={"My TODOs!"} />
+    </div>
+  );
 }
 
 export default App;
--- src/containers/TodoList/TodoList.tsx
+++ src/containers/TodoList/TodoList.tsx
@@ -1,7 +1,28 @@
+import React, { useState } from "react";
+import Todo from "../../components/Todo/Todo";
+
+type TodoType = { id: number; title: string; content: string; done: boolean };
+
 interface IProps {
   title: string;
 }
+
 export default function TodoList(props: IProps) {
   const { title } = props;
-  return <div className="TodoList">{title}</div>;
+  const [todos, setTodos] = useState<TodoType[]>([
+    { id: 1, title: "SWPP", content: "take swpp class", done: true },
+    { id: 2, title: "Movie", content: "watch movie", done: false },
+    { id: 3, title: "Dinner", content: "eat dinner", done: false },
+  ]);
+  return (
+    <div className="TodoList">
+      <div className="title">{title}</div>
+      <div className="todos">
+        Looks like...
+        {todos.map((td) => {
+          return <Todo title={td.title} done={td.done} />;
+        })}
+      </div>
+    </div>
+  );
 }
