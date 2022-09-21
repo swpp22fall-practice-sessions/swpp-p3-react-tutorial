61d9fb594643e61840b29ebfb332f412173de36b
Seungha Jeon <seuha516@naver.com>
2022-09-21 22:43:00 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,25 @@ import NewTodo from "./containers/TodoList/NewTodo/NewTodo";
 import TodoList from "./containers/TodoList/TodoList";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="App">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/todos" element={<TodoList title={"My TODOs!"} />} />
-            {/* <Route path="/todos/:id" element={<TodoDetail />} /> */}
-            <Route path="/new-todo" element={<NewTodo />} />
-            <Route path="/" element={<Navigate replace to={"/todos"} />} />
-            <Route path="*" element={<h1>Not Found</h1>} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        path="/todos"
+                        element={<TodoList title={"My TODOs!"} />}
+                    />
+                    {/* <Route path="/todos/:id" element={<TodoDetail />} /> */}
+                    <Route path="/new-todo" element={<NewTodo />} />
+                    <Route
+                        path="/"
+                        element={<Navigate replace to={"/todos"} />}
+                    />
+                    <Route path="*" element={<h1>Not Found</h1>} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
