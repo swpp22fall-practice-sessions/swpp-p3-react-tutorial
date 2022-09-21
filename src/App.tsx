3f9c8c55ab7ae8eed64cbf3cd979d3963945d19e
SB1491 <etaeyoung2@snu.ac.kr>
2022-09-21 20:57:50 +0900

    9/21

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import TodoList from "./containers/TodoList/TodoList";
 import "./App.css";
+import NewTodo from "./containers/TodoList/NewTodo/NewTodo";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (<div className="App">
+  <BrowserRouter>
+    <Routes>
+      <Route path="todos" element={<TodoList title={"My TODOs!"} />} />
+      <Route path="new-todo" element={<NewTodo />} />
+    </Routes>
+  </BrowserRouter>
+  </div>);
 }
 
 export default App;
