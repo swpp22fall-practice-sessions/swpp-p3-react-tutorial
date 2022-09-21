a49ef586f04f65dce6fdd0e71216a0b616b3590a
You-Been Lee <yblee2001@You-Beens-MacBook-Pro.local>
2022-09-21 21:00:04 +0900

    practice session (9/21)

--- src/containers/TodoList/TodoList.tsx
+++ src/containers/TodoList/TodoList.tsx
@@ -1,1 +1,49 @@
-export {};
+import { useMemo, useState } from 'react';
+import { Todo } from '../../components/Todo/Todo';
+import './TodoList.css'
+import { TodoDetail } from '../../components/TodoDetail/TodoDetail';
+import { NewTodo } from './NewTodo/NewTodo';
+import { Link, NavLink } from 'react-router-dom'
+
+type TodoType = {
+    id: number
+    title: string
+    content: string
+    done: boolean
+}
+
+interface Props {
+    title: string
+}
+
+export const TodoList = (props: Props) => {
+    const { title } = props
+    const [todos, setTodos] = useState<TodoType[]>([
+        { id: 1, title: "SWPP", content: "take swpp class", done: true },
+        { id: 2, title: "Movie", content: "watch movie", done: false },
+        { id: 3, title: "Dinner", content: "eat dinner", done: false },
+    ])
+    const [selectedTodo, setSelectedTodo] = useState<TodoType | null>(null)
+    const todoDetail = useMemo(() => {
+        return selectedTodo ? (<TodoDetail title={selectedTodo.title} content={selectedTodo.content}/>) : null
+    }, [selectedTodo])
+
+    const clickTodoHandler = (td: TodoType) => {
+        setSelectedTodo(td ?? null)
+    }
+
+    return <div className="TodoList">
+        <div className="title">{title}</div>
+        <div className="todos">
+            {todos.map((td) => {
+                return <Todo
+                    title={td.title}
+                    done={td.done}
+                    clicked={() => clickTodoHandler(td)}
+                />
+            })}
+            {todoDetail}
+            <NavLink to={'/new-todo'}>New Todo</NavLink>
+        </div>
+    </div>
+}
