13bd1472936b1f6969d4b1201c693e773a714bf6
Yujin Im <jina0605@gmail.com>
2022-09-21 20:12:01 +0900

    Handle click for TodoList

--- src/containers/TodoList/TodoList.tsx
+++ src/containers/TodoList/TodoList.tsx
@@ -15,13 +15,29 @@ export default function TodoList(props: IProps) {
     { id: 2, title: "Movie", content: "watch movie", done: false },
     { id: 3, title: "Dinner", content: "eat dinner", done: false },
   ]);
+  const [selectedTodo, setSelectedTodo] = useState<TodoType | null>(null);
+
+  const clickTodoHandler = (td: TodoType) => {
+    if (selectedTodo === td) {
+      setSelectedTodo(null);
+    } else {
+      setSelectedTodo(td);
+    }
+  };
+
   return (
     <div className="TodoList">
       <div className="title">{title}</div>
       <div className="todos">
         Looks like...
         {todos.map((td) => {
-          return <Todo title={td.title} done={td.done} />;
+          return (
+            <Todo
+              title={td.title}
+              done={td.done}
+              clicked={() => clickTodoHandler(td)}
+            />
+          );
         })}
       </div>
     </div>
