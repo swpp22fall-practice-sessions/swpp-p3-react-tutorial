6df55a32e90a450d6cbf3f539c64ec31d2ce5755
Yujin Im <jina0605@gmail.com>
2022-09-21 20:30:08 +0900

    Use react-router-dom
    Installed react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NewTodo from "./containers/TodoList/NewTodo/NewTodo";
 import TodoList from "./containers/TodoList/TodoList";
 
 function App() {
   return (
     <div className="App">
-      <TodoList title={"My TODOs!"} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="todos" element={<TodoList title={"My TODOs!"} />} />
+          <Route path="new-todo" element={<NewTodo />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
