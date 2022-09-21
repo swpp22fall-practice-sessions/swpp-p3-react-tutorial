ffd872649389a4b07acc563261ae079256b6f7aa
ongdyub <ongdyub>
2022-09-21 20:54:03 +0900

    create <TodoDetail> with /:id param

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import TodoList from "./containers/TodoList/TodoList";
 import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import NewTodo from "./containers/TodoList/NewTodo/NewTodo";
+import TodoDetail from "./components/TodoDetail/TodoDetail";
 
 function App() {
   return <div className="App">
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="todos" element={<TodoList title={"My TODOs!"} />} />
         <Route path="new-todo" element={<NewTodo />} />
+        <Route path="/todos/:id" element={<TodoDetail />} />
         <Route path="/" element={<Navigate replace to={"/todos"} />} />
         <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
