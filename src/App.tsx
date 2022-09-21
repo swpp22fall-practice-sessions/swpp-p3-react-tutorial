59d1e980a16a39847583974e2ff63cc39f4b23f2
연태영 <dusxodud98@snu.ac.kr>
2022-09-21 19:42:50 +0900

    update codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import "./App.css";
+import TodoList from "./containers/TodoList/TodoList"; // can omit.js
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App"><TodoList title={"My TODOs!"} /></div>;
 }
 
 export default App;
