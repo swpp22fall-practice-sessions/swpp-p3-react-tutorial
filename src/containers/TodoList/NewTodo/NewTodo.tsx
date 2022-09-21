d9fa7c181a9fefe3a0c3635d060f94ecb23353a4
KJYoung <jykim157@snu.ac.kr>
2022-09-21 20:54:55 +0900

    Prac03_React_KJYoung

--- src/containers/TodoList/NewTodo/NewTodo.tsx
+++ src/containers/TodoList/NewTodo/NewTodo.tsx
@@ -1,1 +1,36 @@
-export {};
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+import "./NewTodo.css";
+
+export default function NewTodo(){
+    const [title, setTitle] = useState<string>("");
+    const [content, setContent] = useState<string>("");
+    const [submitted, setSubmitted] = useState<boolean>(false);
+
+    const postTodoHandler = () => {
+        const data = { title: title, content : content};
+        alert("Submitted\n" + data.title + "\n" + data.content);
+        setSubmitted(true);
+    }
+
+    if (submitted){
+        return <Navigate to="/todos" />
+    }else{
+        return (
+            <div className="NewTodo">
+                <h1>Add a Todo</h1>
+                <label>Title</label>
+                <input type="text" value={title} onChange={
+                    (e) => setTitle(e.target.value)
+                }/>
+                <label>Content</label>
+                <textarea rows={4} value={content} onChange={
+                    (e) => setContent(e.target.value)
+                }/>
+                <button onClick={
+                    () => postTodoHandler()
+                }>Submit</button>
+            </div>
+        )
+    }
+}
