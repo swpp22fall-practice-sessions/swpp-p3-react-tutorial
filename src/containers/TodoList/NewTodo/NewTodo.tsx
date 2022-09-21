8471e93e6fceabdfdfb719b1d771fb22959b10db
kl7407 <eee7407@naver.com>
2022-09-21 20:12:00 +0900

    Update: follow up lab3 introduction.

--- src/containers/TodoList/NewTodo/NewTodo.tsx
+++ src/containers/TodoList/NewTodo/NewTodo.tsx
@@ -1,1 +1,45 @@
-export {};
+import "./NewTodo.css"
+import { Navigate } from "react-router-dom";
+import { useState } from "react";
+
+export default function NewTodo(){
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+  const [submitted, setSubmitted] = useState<boolean>(false);
+
+
+  const postTodoHandler = () => {
+    const data = { title: title, content: content };
+    alert("Submitted\n" + data.title + "\n" + data.content);
+    setSubmitted(true);
+  };
+
+
+
+  if (submitted)
+    return (<Navigate to="/todos" />)
+  else
+    return (
+      <div className="NewTodo">
+        <h1>Add a Todo</h1>
+        <label>Title</label>
+        <input
+          type="text"
+          value={title}
+          onChange={
+            (event) => setTitle(event.target.value)
+          }
+        />
+        <label>Content</label>
+        <textarea
+          rows={4}
+          value={content}
+          onChange={
+            (event) => setContent(event.target.value)
+          }
+        >
+        </textarea>
+        <button onClick={postTodoHandler}>Submit</button>
+      </div>
+    )
+};
