711f9a4d70f72c7d18b73082efa8d31f6635cd83
huGgW <whjoon0225@naver.com>
2022-09-21 22:18:46 +0900

    swpp lab3 commit

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,1 +1,20 @@
-export {};
+import "./Todo.css"
+
+interface Iprops {
+    title: string;
+    clicked?: React.MouseEventHandler<HTMLDivElement>;
+    done: boolean;
+}
+
+const Todo = (props: Iprops) => {
+    return (
+        <div className="Todo">
+            <div className={'text ${props.done && "done"}'} onClick={props.clicked}>
+                {props.title}
+            </div>
+            {props.done && <div className="done-mark">&#x2713;</div>}
+        </div>
+    );
+};
+
+export default Todo;
