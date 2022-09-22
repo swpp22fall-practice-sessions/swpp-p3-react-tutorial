92a919b01b51244f34f3b1fc3090d843ff52a7f8
notiona <notiona@snu.ac.kr>
2022-09-22 20:48:46 +0900

    0921 practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import {BrowserRouter, Routes, Route } from "react-router-dom";
+
+import TodoList from "./containers/TodoList/TodoList";
+import NewTodo from "./containers/TodoList/NewTodo/NewTodo";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <BrowserRouter>
+      <Routes>
+        <Route path="todos" element={<TodoList title={"My TODOs!"}/>}></Route>
+        <Route path="new-todo" element={<NewTodo />}></Route>
+      </Routes>
+    </BrowserRouter>
+  </div>;
 }
 
 export default App;
--- src/containers/TodoList/TodoList.tsx
+++ src/containers/TodoList/TodoList.tsx
@@ -1,1 +1,53 @@
-export {};
+import {useState, useMemo} from "react";
+import { Link, NavLink } from "react-router-dom";
+
+import Todo from '../../components/Todo/Todo';
+import TodoDetail from "../../components/TodoDetail/TodoDetail";
+import NewTodo from "./NewTodo/NewTodo";
+
+import "./TodoList.css";
+
+type TodoType = {
+    id: number;
+    title: string;
+    content: string;
+    done: boolean;
+};
+
+interface IProps {
+    title: string;
+};
+
+export default function TodoList(props: IProps){
+    const {title} = props;
+    const [todos, setTodos] = useState<TodoType[]>(
+        [
+            {id: 1, title: "laundry", content: "to the laundry", done:true},
+            {id: 2, title: "dishes", content: "to the dishes", done:false},
+        ]
+    );
+    const [selectedTodo, setSelectedTodo] = useState<TodoType | null>(null);
+    const clickTodoHandler = (todo: TodoType) => {
+        if (selectedTodo === todo) {
+            setSelectedTodo(null);
+        }
+        else {
+            setSelectedTodo(todo);
+        }
+    };
+    const todoDetail = useMemo(() => {
+        return selectedTodo ? (<TodoDetail title={selectedTodo.title} content={selectedTodo.content} />) : null;
+    }, [selectedTodo]);
+    return <div className="TodoList">
+        <div className="title">{title}</div>
+        <div className="todos">
+            {todos.map((td) => {
+                return <Todo key={td.id} title={td.title} done={td.done} clicked={() => clickTodoHandler(td)}/>
+            }
+            )}
+            {todoDetail}
+            <NavLink to="/new-todo">New Todo</NavLink>
+            <NewTodo />
+        </div>
+    </div>;
+};
